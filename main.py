7e6dcfd2b19c35143491a399f1e0204f6185d0c9
JeSuisUneFougere <turp.henry@gmail.com>
2018-06-19 22:13:51 +0200

    Read token from file.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+TOKEN = ""
+
+
 def main():
-    print("Hello world")
+    # File has to be in same folder as main.py.
+    with open("token.txt", "r") as f:
+        for line in f:
+            TOKEN = line
+            break  # There should only be one line in this file anyway.
+    print(TOKEN)
 
 
 if __name__ == "__main__":
