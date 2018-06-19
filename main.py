aaf1db30d037327d456e02de3efc0340555c8386
monica <monica.stanescu95@gmail.com>
2018-06-19 23:29:02 +0300

    Created get_token() function

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-TOKEN = ""
 
-
-def main():
+def get_token():
     # File has to be in same folder as main.py.
     with open("token.txt", "r") as f:
         for line in f:
-            TOKEN = line
+            token = line
             break  # There should only be one line in this file anyway.
-    print(TOKEN)
+    return token
+
+
+def main():
+    token = get_token()
+    print(token)
 
 
 if __name__ == "__main__":
