917b6258d16edda1a21daf34c8597c7e648d5630
monica <monica.stanescu95@gmail.com>
2018-06-23 23:54:48 +0300

    .

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import json
 import requests
 
-TOKEN = ""
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-
 
 def get_token():
     # File has to be in same folder as main.py.
     with open("token.txt", "r") as f:
         for line in f:
             return line
+
+
+BASE_URL = "https://api.telegram.org/bot{}/".format(get_token())
 
 
 def get_content(url):
@@ -23,19 +23,17 @@ def get_json_from_url(url):
 
 
 def compute_updates_url():
-    return URL + "getUpdates"
+    return BASE_URL + "getUpdates"
 
 
 def get_updates():
     updates_url = compute_updates_url()
-    print("Getting updates from: "+updates_url)
+    print("Getting updates from: " + updates_url)
     json_object = get_json_from_url(updates_url)
     return json_object
 
 
 def main():
-    TOKEN = get_token()
-    print("Token:" + TOKEN)
     print(get_updates())
 
 
